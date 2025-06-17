ac81c88a0fe747675595e61c1d0eca9bd360d788
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-06-17 11:59:53 +0530

    Added Sidebar and SearchFeed components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Loader} from './Loader';
+export {default as Navbar} from './Navbar';
+export {default as SearchBar} from './SearchBar';
+export {default as SideBar} from './SideBar';
+export {default as SearchFeed} from './SearchFeed';
